d9f6f813eb521b101028aa787d80baa7dee547b2
FBS2307 <69609356+Bittner2307@users.noreply.github.com>
2021-06-21 22:04:19 -0300

    version0.1.1

--- app/src/main/java/com/example/frasesfracasso/MainActivity.java
+++ app/src/main/java/com/example/frasesfracasso/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.frasesfracasso;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,34 +16,40 @@ public class MainActivity extends AppCompatActivity {
 
     public void gerarNovaFrase(View view){
         String [] frases = {
-            "NADA É TÃO RUIM QUE NÃO POSSA PIORAR !",
-            "FRACASSO É UM ESTADO DE ESPÍRITO !",
-            "NUNCA É TARDE PARA DESISTIR !",
-            "SEU MAIOR PROBLEMA É VOCE MESMO !",
-            "SABENDO QUE ERA IMPOSSIVEL ,FOI LÁ E SOUBE !",
-            "CALMA. TUDO PASSA,NEM QUE SEJA EM CIMA DE VOCE !",
-            "ACREDITE , VOCE NASCEU PARA CONQUISTAR VÁRIOS FRACASSOS !",
-            "O CAMINHO É LONGO,MAS A DERROTA É CERTA !",
-            "É SÓ UMA FASE RUIM ,LOGO VAI PIORAR !",
-            "NUNCA FOI AZAR SEMPRE FOI A INCOMPETENCIA !",
-            "SEJA PROTAGONISTA DO SEU FRACASSO !",
-            "VOCE É MAIS FRACO DO QUE PENSA !",
-            "NADA COMO UM DIA PIOR DO QUE O OUTRO ! ",
-            "O UNIVERSO ESTÁ POUCO SE FODENDO PARA VOCE !",
-            "O RECOMEÇO É OPORTUNIDADE PARA UM GRANDE FRACASSO !",
-            "A VIDA TE DERRUBA HOJE PREPARANDO PARA QUEDA DE AMANHÃ !",
-            "RECLAME MENOS, FRACASSE MAIS !",
-            "DURMA 12 HORAS POR DIA ,ASSIM AS CHANCES DE FAZER MERDA CAEM PELA METADE!",
-            "TUDO ESTÁ DANDO ERRADO  CONFORME O ESPERADO!",
-            "NUNCA DEIXE QUE NINGUÉM DIGA QUE VOCE NÃO CONSEGUE,DIGA VOCE MESMO: EU NÃO CONSIGO ! ",
-            "A VIDA É UM CONTO DE FALHAS!",
-            "LUTE COMO NUNCA ,FALHE COMO SEMPRE !",
-            "O IMPORTANTE NÃO É LUTAR SEMPRE,MAS PERDER TODOS OS DIAS !",
-            "VOCE É FRACASSADO PULE DE UM PRÉDIO !",
-
-
+            "  NADA É TÃO RUIM QUE NÃO POSSA PIORAR !",
+            "  FRACASSO É UM ESTADO DE ESPÍRITO !",
+            "  NUNCA É TARDE PARA DESISTIR !",
+            "  SEU MAIOR PROBLEMA É VOCE MESMO !",
+            "  SABENDO QUE ERA IMPOSSIVEL ,FOI LÁ E SOUBE !",
+            "  CALMA. TUDO PASSA,NEM QUE SEJA EM CIMA DE VOCE !",
+            "  ACREDITE , VOCE NASCEU PARA CONQUISTAR VÁRIOS FRACASSOS !",
+            "  O CAMINHO É LONGO,MAS A DERROTA É CERTA !",
+            "  É SÓ UMA FASE RUIM ,LOGO VAI PIORAR !",
+            "  NUNCA FOI AZAR SEMPRE FOI A INCOMPETENCIA !",
+            "  SEJA PROTAGONISTA DO SEU FRACASSO !",
+            "  VOCE É MAIS FRACO DO QUE PENSA !",
+            "  NADA COMO UM DIA PIOR DO QUE O OUTRO ! ",
+            "  O UNIVERSO ESTÁ POUCO SE FODENDO PARA VOCE !",
+            "  O RECOMEÇO É OPORTUNIDADE PARA UM GRANDE FRACASSO !",
+            "  A VIDA TE DERRUBA HOJE PREPARANDO PARA QUEDA DE AMANHÃ !",
+            "  RECLAME MENOS, FRACASSE MAIS !",
+            "  DURMA 12 HORAS POR DIA ,ASSIM AS CHANCES DE FAZER MERDA CAEM PELA METADE!",
+            "  TUDO ESTÁ DANDO ERRADO  CONFORME O ESPERADO!",
+            "  NUNCA DEIXE QUE NINGUÉM DIGA QUE VOCE NÃO CONSEGUE,DIGA VOCE MESMO: EU NÃO CONSIGO ! ",
+            "  A VIDA É UM CONTO DE FALHAS!",
+            "  LUTE COMO NUNCA ,FALHE COMO SEMPRE !",
+            "  O IMPORTANTE NÃO É LUTAR SEMPRE,MAS PERDER TODOS OS DIAS !",
+            "  VOCE É FRACASSADO PULE DE UM PRÉDIO !",
+            "  O NÃO VOCÊ JÁ TEM,BUSQUE A HUMILHAÇÃO !!",
+            "  SE UMA COISA TEM A CHANCE DE DAR ERRADO ... DARÁ !!",
+            "  ACRADITAR QUE VOCÊ PODE JAÉ MEIOCAMINHO ANDADO PARA O FRACASSO!!",
+            " NÃO TENHA MEDO DE FRACASSO, TENHA COSTUME !!",
+            " DAQUI A UM ANO VOCÊ VAI DESEJAR TER DESISTIDO HOJE !! !!",
+            " SEM LUTAS NÃO HÁ DERROTAS !!",
+            " VOCÊ NÃO PODE MUDAR O PASSADO MAS PODE ESTRAGAR O SEU FUTURO  !!",
+            " ENQUANTO HOUVER ESPERANÇA NÃO HAVERÁ SOLUÇÃO !!",
         };
-        int numero = new Random().nextInt(23);//gera randomicamente os numeros (frases)
+        int numero = new Random().nextInt(32);//gera randomicamente os numeros (frases)
         TextView texto = findViewById(R.id.textResultado);
         texto.setText(frases[numero]);
     }
